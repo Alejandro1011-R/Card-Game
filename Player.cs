564553b0e7988d1b96c3943d8a94ba548f55335c
AleTheCreation <alejandrito110402@gmail.com>
2022-12-09 20:58:43 -0500

    a4

--- Player.cs
+++ Player.cs
@@ -4,12 +4,28 @@ namespace BattleCards
 {
     public class Player
     {
+        private string Name;
+        public Player(string name)
+        {
+            Name = name;
+            PlayerM = new List<Card>();
+            PlayerR = new List<Card>();
+            Cementery = new List<Card>();
+            PlayerHand = new List<Card>();
+            Deck = new List<Card>();
+            Pass = false;
+        }
         public List<Card> PlayerM; //melee row// fila de ataque cuerpo a cuerpo (max 9)
         public List<Card> PlayerR; //range row// ataque larga distancia (max 9)
         public List<Card> Cementery;
         public List<Card> PlayerHand; 
         public List<Card> Deck;
         public bool Pass;
+
+        public string GetName()
+        {
+            return Name;
+        }
 
     }   
 }
