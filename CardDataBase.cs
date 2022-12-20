fa5e2112824a5070c9e66d91345f67aa70c25596
AleTheCreation <alejandrito110402@gmail.com>
2022-12-20 17:03:44 -0500

    Update 1.0.18

--- CardDataBase.cs
+++ CardDataBase.cs
@@ -10,7 +10,13 @@ namespace BattleCards
 
 
         public void awake()
-        {  //int id, string name, string description, int power, int armour, int cardType, int faction, int colour
+        { 
+            var aux = new tokenizer("(Almeja) [Lo ultimo de la nueva generacion] power 4 faccion 2 que QuitePoder 6 SubePoder 1");
+            var aux2= new parser(aux);
+
+            CardList.Add(aux2.CreateCard());
+            
+             //int id, string name, string description, int power, int armour, int cardType, int faction, int colour
             //faction 0 skelije
             //faction 1 monstruos
             //0 oro, 1 plata, 2  bronze (colour)
