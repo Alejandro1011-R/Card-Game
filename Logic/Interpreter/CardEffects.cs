4ce4da2e9d4778002c8291ce003a7ee56943cb6b
AleTheCreation <alejandrito110402@gmail.com>
2022-12-23 16:07:55 -0500

    Update 1.0.26  Reorganize

---
+++ Logic/Interpreter/CardEffects.cs
@@ -0,0 +1,61 @@
+using System.ComponentModel.DataAnnotations;
+using System.Reflection.Emit;
+
+namespace BattleCards
+{
+    public abstract class effecto{
+
+        public virtual void effect(GameRun game){}
+    }
+
+    class QuitarPower :effecto
+    {
+        private int CantPower;
+        public QuitarPower(int power) 
+        {
+            CantPower=power; 
+        }
+
+        public override void effect(GameRun game)
+        {
+            if(GameRun.PlayerOpposide.Hand.Count()>0)
+            {
+                int id=int.Parse(Console.ReadLine()!);
+                foreach(var carta in GameRun.PlayerOpposide.PlayerM)
+                {
+                    if(carta.Id==id)
+                    {
+                        carta.Power-=CantPower;
+                        return;
+                    }
+                }
+            }
+
+        }
+
+    }
+
+    class SubirPoder :effecto
+    {
+        private int CantPower;
+
+        public SubirPoder(int power)
+        {
+            CantPower=power;
+        }
+        public override void effect(GameRun game)
+        {
+            
+            int id=int.Parse(Console.ReadLine()!);
+            foreach(var carta in GameRun.PlayerInTurn.PlayerM)
+            {
+                if(carta.Id==id)
+                {
+                    carta.Power-=CantPower;
+                    return;
+                }
+            }
+        }
+
+    }
+}
